4e04b997f567c711411e3e6026f0aa8ff02e1683
Bruno Pontes <brunodeveloper18@gmail.com>
2017-08-19 08:27:09 -0300

    Componentization of navbar

--- app/components/navbar-component.js
+++ app/components/navbar-component.js
@@ -1,17 +1,12 @@
-let html = '<navbar>'
-html += '<section id="navbar-options">'
-html += '<section id="navbar-logo">'
-html += '<img src="app/assets/images/instagram-logo.png" width="150" />'
-html += '</section>'
+let html = '<section id="navbar-options">'
 html += '<ul>'
 html += '<li><a href="#">Users</a></li>'
 html += '<li><a href="#">Tags</a></li>'
 html += '<li><a href="#">Location</a></li>'
 html += '</ul>'
 html += '<section>'
-html += '</navbar>'
 
-Vue.component('instavue-navbar', {
+Vue.component('instavue-navbar-options', {
     template: html
 })
 
---
+++ app/components/navbar-logo-component.js
@@ -0,0 +1,11 @@
+let content = '<section id="navbar-logo">'
+content += '<img src="app/assets/images/instagram-logo.png" width="150" />'
+content += '</section>'
+
+Vue.component('instavue-logo', {
+    template: content
+})
+
+new Vue({
+    el: '#navbar'
+})
