2c482582a9441f97e3191d250231fa47157ef3d0
Bruno Pontes <brunodeveloper18@gmail.com>
2017-08-19 14:28:56 -0300

    Search bar component

---
+++ app/components/content-searchbar-component.js
@@ -0,0 +1,7 @@
+Vue.component('instavue-searchbar', {
+    template: '<input type="text" name="search-bar" id="search-bar" placeholder="Insira o termo para a busca" />'
+})
+
+new Vue({
+    el: '#content'
+})
