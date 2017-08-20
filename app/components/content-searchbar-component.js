403ee42a9a7ca688f789ba1e24a67f1add6135ba
Bruno Pontes <brunodeveloper18@gmail.com>
2017-08-19 22:00:40 -0300

    Getting the Instagram API code response

--- app/components/content-searchbar-component.js
+++ app/components/content-searchbar-component.js
@@ -1,5 +1,8 @@
+let searchbar = '<section id="search-bar"><input type="text" name="search-bar" placeholder="Insira o termo para a busca" />'
+searchbar += '<button class="search-term" id="searchTerm">🔍</button></section>'
+
 Vue.component('instavue-searchbar', {
-    template: '<input type="text" name="search-bar" id="search-bar" placeholder="Insira o termo para a busca" />'
+    template: searchbar
 })
 
 new Vue({
--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const auth = require(path.join(__dirname, 'app/modules/auth.js'))()
 const port = 3000
 
 app.use(express.static('app'))
 app.use(express.static('node_modules'))
 
-app.get ('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')))
+/*
+* AUTH
+*/
+
+app.use('/instagram/oauth', (req, res) => {
+    const code = req.query.code
+    console.log(code)
+})
+
+/*
+* ENDPOINTS
+*/
+
+app.get ('/', [(req, res) => {
+    
+    auth.init()
+    .then(result => res.redirect(result))//res.redirect(result))
+    .catch(e => console.log(e))
+}], (req, res) => res.sendFile(path.join(__dirname, 'index.html')))
+
+/*
+* RUN SERVER
+*/
 
 app.listen(port, () => `Running at port ${3000}`)
